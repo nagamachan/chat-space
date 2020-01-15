52a236cbefabfff94e52bd096b1bbb68a5762a7d
nagamachan <59175719+nagamachan@users.noreply.github.com>
2020-01-15 16:09:55 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -61,11 +61,11 @@ $(function(){
      })
      .fail(function() {
       alert("メッセージ送信に失敗しました");
+    });
   });
     var reloadMessages = function() {
     //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
       last_message_id = $('.message:last').data("message-id");
-    
       $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
@@ -99,5 +99,4 @@ $(function(){
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
-  });
 });
